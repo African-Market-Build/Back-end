0f6d510e58bfb5bfd87e5ef7f2e6b6b174d4beb6
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-31 15:10:27 -0500

    created table for items

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,12 +1,28 @@
 exports.up = async (knex) => {
-  await knex.schema.createTable("owners", (owners) => {
-    owners.increments("owner_id");
-    owners.string("username", 200).notNullable().unique();
-    owners.string("password", 200).notNullable();
-    owners.timestamps(false, true);
-  });
+  await knex.schema
+    .createTable("owners", (owners) => {
+      owners.increments("owner_id");
+      owners.string("username", 200).notNullable().unique();
+      owners.string("password", 200).notNullable();
+      owners.timestamps(false, true);
+    })
+    .createTable("items", (items) => {
+      items.increments("items_id");
+      items.string("item_name", 200).notNullable();
+      items.string("item_location", 200).notNullable();
+      items.string("item_description", 200).notNullable();
+      items.integer("items_price").notNullable();
+      items.integer("available").defaultsTo(false);
+      items
+        .integer("owner_id")
+        .notNullable()
+        .references("owner_id")
+        .inTable("owners")
+        .unSigned();
+    });
 };
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("items");
   await knex.schema.dropTableIfExists("owners");
 };
