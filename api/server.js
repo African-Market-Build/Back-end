e61538c294e7dec00527f5c9c34165a881dcfd49
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-31 18:30:09 -0500

    added restriction for items

--- api/server.js
+++ api/server.js
@@ -3,13 +3,14 @@ const helmet = require("helmet");
 const cors = require("cors");
 const authRouter = require("./auth/authRouter");
 const itemsRouter = require("./items/itemsRouter");
+const restricted = require("./items/restricted");
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use("/api/auth", authRouter);
-server.use("/api/items", itemsRouter);
+server.use("/api/items", restricted, itemsRouter);
 
 server.get("/", (req, res) => {
   res.send("Welcome hahaha");
