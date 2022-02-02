d66706556d7d0c21c62e01051658030e2bd1d8da
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-02-01 23:56:26 -0500

    seed

--- api/data/seeds/items.js
+++ api/data/seeds/items.js
@@ -7,7 +7,13 @@ exports.seed = function (knex) {
   return knex("owners").then(() => {
     // Inserts seed entries
     return knex("owners")
-      .insert([{ username: "javier", password: "1234" }])
+      .insert([
+        { username: "javier", password: "1234" },
+        { username: "laysha", password: "1234" },
+        { username: "jay", password: "1234" },
+        { username: "johny", password: "1234" },
+        { username: "rebecca", password: "1234" },
+      ])
 
       .then(() => {
         return knex("items").insert([
@@ -18,6 +24,38 @@ exports.seed = function (knex) {
             item_price: 300,
             available: true,
             owner_id: 1,
+          },
+          {
+            item_name: "  PS5",
+            item_location: "philly",
+            item_description: "a device I am overcharging for",
+            item_price: 1200,
+            available: true,
+            owner_id: 2,
+          },
+          {
+            item_name: "Jordan 1s",
+            item_location: "Florida",
+            item_description: "My favorite pair of sneaks",
+            item_price: 900,
+            available: true,
+            owner_id: 3,
+          },
+          {
+            item_name: "MacBook Pro",
+            item_location: "Georgia",
+            item_description: "an amazing device I use for coding",
+            item_price: 1400,
+            available: true,
+            owner_id: 4,
+          },
+          {
+            item_name: "Painting",
+            item_location: "philly",
+            item_description: "an ancient piece of art",
+            item_price: 12000,
+            available: true,
+            owner_id: 5,
           },
         ]);
       });
